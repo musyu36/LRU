b53254438e16d51dca808c37b1015f4e93cf34ac
mimura <musyu36@gmail.com>
2019-10-10 18:10:38 +0900

    reset and comment out

--- lru.js
+++ lru.js
@@ -1,12 +1,21 @@
 class LRU{
-    constructor() {
-        this.cache = {};
-    }
-    put(key, data) {
-        this.cache[key] =  data;
-    }
-    get(key) {
-        return this.cache[key];
-    }
+    
+    // constructor() {
+    //     this.cache = {};
+    // }
+    // put(key, data) {Object.keys(lru.cache).length
+    //     this.cache[key] = data;
+    //     if (Object.keys(lru.cache).length >= 3) {
+    //         let keys_array = Object.keys(this.cache);
+    //         let len = keys_array.length;
+    //         // delete this.cache[keys_array[len - 1]];
+    //         // delete this.cache['a'];
+    //         delete this.cache[0];
+    //         console.log("deleted");
+    //     }
+    // }
+    // get(key) {
+    //     return this.cache[key];
+    // }
 }
 module.exports = LRU;
--- lru.test.js
+++ lru.test.js
@@ -1,26 +1,36 @@
 const LRU = require("./lru");
 
-// クラスの用意
-test("create new array", () => {
-    lru = new LRU();
-    lru.put("a" , "dataA");
-    expect(Object.keys(lru).length).toBe(1);
-});
+// // クラスの用意
+// test("create new array", () => {
+//     lru = new LRU();
+//     expect(Object.keys(lru.cache).length).toBe(0);
+// });
 
-// 要素の追加
-test("put the element", () => {
-    lru = new LRU();
-    lru.put("a", "dataA");
-    lru.put("b" , "dataB");
-    expect(lru.cache["a"]).toBe("dataA");
-    expect(lru.cache["b"]).toBe("dataB");
-});
+// // 要素の追加
+// test("put the element", () => {
+//     lru = new LRU();
+//     lru.put("a", "dataA");
+//     lru.put("b" , "dataB");
+//     expect(lru.cache["a"]).toBe("dataA");
+//     expect(lru.cache["b"]).toBe("dataB");
+// });
 
-// 要素の表示
-test("display the element", () => {
-    lru = new LRU();
-    lru.put("a", "dataA");
-    lru.put("b", "dataB");
-    expect(lru.get("a")).toBe("dataA");
-    expect(lru.get("b")).toBe("dataB");
-});
+// // 要素の表示
+// test("display the element", () => {
+//     lru = new LRU();
+//     lru.put("a", "dataA");
+//     lru.put("b", "dataB");
+//     expect(lru.get("a")).toBe("dataA");
+//     expect(lru.get("b")).toBe("dataB");
+// });
+
+// // 使われていない要素を削除する
+// test("display the element", () => {
+//     lru = new LRU();
+//     lru.put("a", "dataA");
+//     lru.put("b", "dataB");
+//     lru.put("c", "dataC");
+//     console.log(lru.get("a"));
+//     console.log(lru);
+//     expect(lru.cache).not.toContain("a");
+// });
