7b3669ac0ab522a7d060134107076fd7a38e15f3
mimura <musyu36@gmail.com>
2019-10-14 18:34:47 +0900

    update file

--- lru.js
+++ lru.js
@@ -1,3 +1,4 @@
+// 要素の順番を入れ替える
 function moveAt(array, index, at) {
     if (index === at || index > array.length - 1 || at > array.length - 1) {
         return array;
@@ -16,20 +17,26 @@ function moveAt(array, index, at) {
 }
 
 class LRU{
+    // 配列の準備
     constructor() {
         this.cache = [];
     }
+
+    // 要素を追加する
     put(key, data) {
         this.cache.push({[key]: data });
 
+        // 配列の要素数が３以上になると先頭の要素を削除する
         if (this.cache.length >= 3) {
             this.cache.shift();
         }
     }
+    // 要素を取得する
     get(x) {
         var for_return;
         var cnt = 0;
 
+        // 引数とキーが合致する要素を探す
         this.cache.forEach(function (value) {
             for (var key in value) {
                 if (key === x) {
@@ -41,6 +48,7 @@ class LRU{
                 cnt++;
             }
         });
+        // 取得された要素を配列の末尾に挿入する
         this.cache = moveAt(this.cache, cnt, this.cache.length - 1);
         return for_return;
     }
